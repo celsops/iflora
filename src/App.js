ac9d2f6e33372ed2ca4d8b0d2067cd4e7ea1019c
celsops <celsovasconcelos.27@gmail.com>
2020-01-04 22:13:07 -0300

    Tela de boas vindas como foi proposta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Nav,Navbar,Image, Carousel } from 'react-bootstrap';
+import logo from './static/img/logo.svg'; 
+import fundo from './static/img/fundo.svg';
+import './static/css/App.css';  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div>
+            <Navbar className="navbar" expand="lg" fixed="top">
+                <Navbar.Brand href="#home">
+                    <Image src={logo} className="logo" rounded></Image>
+                </Navbar.Brand>
+                <Navbar.Toggle  aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+                    <Nav className="mr-right">
+                        <Nav.Link href="#inicio" className="nav-ativo" >Inicio</Nav.Link>
+                        <Nav.Link href="#sobre">Sobre nós</Nav.Link>
+                        <Nav.Link href="#login">Loign</Nav.Link>
+                        <Nav.Link href="#cadastro">Cadastre-se</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Carousel>
+                <Carousel.Item>
+                    <Image
+                        src={fundo}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption style={{bottom:"240px"}}>
+                        <h3>Sistema de cadastramento e publicações de eventos</h3>
+                        <button className="btn-saber-mais">Saber mais</button>
+                    </Carousel.Caption>
+                </Carousel.Item>    
+            </Carousel>
+        </div>
     </div>
   );
 }
